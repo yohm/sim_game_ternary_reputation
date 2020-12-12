53e2bb465d22b099f67f3d58cf9a075b92b5fe44
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-12-13 00:37:15 +0900

    test random iteration

--- test_ReputationDynamics.cpp
+++ test_ReputationDynamics.cpp
@@ -64,5 +64,53 @@ int main(int argc, char *argv[]) {
     assert( rd != rd2 );
   }
 
+  {
+    // when GG=>C is fixed, there are 255 action rules
+    for (size_t i = 0; i < 256; i++) {
+      ActionRule ar(i + 256);
+      assert(ar.ActAt(Reputation::G, Reputation::G) == Action::C);
+    }
+  }
+
+  {
+    // when GGC => G and GGD => B are fixed, there are 3^16 = 43046721 types of reputation dynamics
+    // add 2*3^17 + 0*3^16 = 258280326
+    uint64_t fixed = 258280326ull;
+    for (size_t i = 0; i < 43046721; i+=100) {
+      if (i % 10000000 == 0) { std::cerr << i << std::endl; }
+      ReputationDynamics rd(fixed + i);
+      assert(rd.RepAt(Reputation::G, Reputation::G, Action::C) == Reputation::G);
+      assert(rd.RepAt(Reputation::G, Reputation::G, Action::D) == Reputation::B);
+    }
+
+    {
+      // when GGC => G and GGD => B are fixed, there are 3^16 = 43046721 types of reputation dynamics
+      // add 2*3^17 + 0*3^16 = 258280326
+      uint64_t fixed = 258280326ull;
+      uint64_t i = 21046724;
+      ReputationDynamics rd(fixed + i);
+      ActionRule base(511);  // use AllC as a baseline
+      std::vector< std::pair<Reputation,Reputation> > free_pairs;
+      for (int i = 0; i < 3; i++) {
+        Reputation X = static_cast<Reputation>(i);
+        for (int j = 0; j < 3; j++) {
+          Reputation Y = static_cast<Reputation>(j);
+          if (X == Reputation::G && Y == Reputation::G) continue;
+          if (rd.RepAt(X, Y, Action::C) == rd.RepAt(X, Y, Action::D)) {
+            base.SetAction(X,Y, Action::D);
+          }
+          else {
+            free_pairs.emplace_back( std::make_pair(X,Y) );
+          }
+        }
+      }
+      std::cerr << rd.Inspect();
+      std::cerr << base.Inspect();
+      for (auto p : free_pairs) {
+        std::cerr << p.first << ' ' << p.second << std::endl;
+      }
+    }
+  }
+
   return 0;
 }
