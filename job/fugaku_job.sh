67f70e055ca30c6d99cb94fcb2790531d998b434
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-12-23 14:46:06 +0900

    add jobscript for fugaku

---
+++ job/fugaku_job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PJM --rsc-list "node=2000"
+#PJM --rsc-list "rscunit=rscunit_ft01"
+#PJM --rsc-list "rscgrp=eap-large"
+#PJM --rsc-list "elapse=03:00:00"
+#PJM --mpi "max-proc-per-node=4"
+#PJM -s
+
+export OMP_NUM_THREADS=12
+
+mpiexec -stdout-proc ./%/1000R/stdout -stderr-proc ./%/1000R/stderr ../main.out
