2012a848f0f27957d63d07aad160cdacc3ad6bc0
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-12-24 18:23:43 +0900

    print independent ReputationDynamics list

---
+++ print_normalized_RD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include "ReputationDynamics.hpp"
+
+
+int main( int argc, char* argv[]) {
+
+  uint64_t max = 387'420'489ull;  // 3**18
+
+  std::vector<uint64_t> ans;
+  for (uint64_t i = 0; i < max; i++) {
+    if (i % 1'000'000 == 0) { std::cerr << i << std::endl; }
+    ReputationDynamics rd(i);
+    if (rd.Normalized().first.ID() == i) {
+      ans.push_back(i);
+    }
+  }
+
+  for (uint64_t a : ans) {
+    std::cout << a << "\n";
+  }
+  std::cerr << ans.size() << std::endl;
+
+  return 0;
+}
