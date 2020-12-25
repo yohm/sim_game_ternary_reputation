68df96528ec566eaf2a3e48ca729dd52e5edf5f3
Yohsuke Murase <yohsuke.murase@gmail.com>
2020-12-26 00:08:42 +0900

    store results to vector

--- analyze_ESS.cpp
+++ analyze_ESS.cpp
@@ -60,13 +60,19 @@ int main(int argc, char* argv[]) {
 
 
   const double mu_e = 0.02, mu_a = 0.02;
-  for (size_t i = 0; i < game_ids.size(); i++) {
+  const size_t N = game_ids.size();
+  std::vector<uint64_t > new_game_ids(N, 0ull);
+  std::vector<double> coop_probs(N, 0.0);
+  std::vector<std::array<double,3> > hs(N);
+
+  for (size_t i = 0; i < N; i++) {
     if (i % 10'000 == 0) { std::cerr << i << " / " << game_ids.size() << std::endl; }
     Game g(mu_e, mu_a, game_ids[i]);
     // std::cerr << g.rep_dynamics.ID() << ' ' << (game_ids[i] >> 9ull) << std::endl;
     // rep_ids.push_back( g.rep_dynamics.ID() );
     double cprob = g.ResidentCoopProb();
     auto h = g.ResidentEqReputation();
+
     std::array<int,3> map;
     if (h[0] >= h[1] && h[1] >= h[2]) { map = {2,1,0}; }
     else if (h[0] >= h[2] && h[2] >= h[1]) { map = {2, 0, 1}; }
@@ -75,18 +81,25 @@ int main(int argc, char* argv[]) {
     else if (h[2] >= h[1] && h[1] >= h[0]) { map = {0, 1, 2}; }
     else if (h[2] >= h[0] && h[0] >= h[1]) { map = {1, 0, 2}; }
     else { throw std::runtime_error("must not happen"); }
+
     ReputationDynamics new_repd = g.rep_dynamics.Permute(map);
     ActionRule new_ar = g.resident_ar.Permute(map);
     Game new_g(mu_e, mu_a, new_repd, new_ar);
+
+    new_game_ids[i] = new_g.ID();
+    coop_probs[i] = cprob;
     std::array<double,3> new_h = {0.0};
-    for (size_t i = 0; i < 3; i++) { new_h[ map[i] ] = h[i]; }
-    std::cout << g.ID() << ' ' << new_g.ID() << ' ' << cprob << ' ' << new_h[0] << ' ' << new_h[1] << ' ' << new_h[2] << std::endl;
+    for (size_t n = 0; n < 3; n++) { hs[i][ map[n] ] = h[n]; }
     /*
     if (h[0] < 0.8 && h[1] < 0.8 && h[2] < 0.8 && cprob > 0.95) {
       std::cout << game_ids[i] << ' ' << cprob << ' ' << h[0] << ' ' << h[1] << ' ' << h[2] << std::endl;
       std::cout << g.Inspect();
     }
      */
+  }
+
+  for (size_t i = 0; i < N; i++) {
+    std::cout << game_ids[i] << ' ' << new_game_ids[i] << ' ' << coop_probs[i] << ' ' << hs[i][0] << ' ' << hs[i][1] << ' ' << hs[i][2] << std::endl;
   }
 
   return 0;
