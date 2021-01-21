076519066dfa8732f3a4eb1192badc30a140055a
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-01-21 17:29:58 +0900

    [refactoring] removed obsolete method

--- Game.hpp
+++ Game.hpp
@@ -65,44 +65,6 @@ class Game {
     }
     return true;
   }
-  bool IsLikeLeadingEight() const {
-    ReputationDynamics rd = rep_dynamics;
-    ActionRule ar = resident_ar;
-
-    // Common rules of leading eight
-    //- GGc => G
-    //- GGd => B
-    //- GBd => G
-    //- BGc => G
-    //- BGd => B (not G)
-    //- GG => c
-    //- GB => d
-    //- BG => c
-
-    // GGc => G and GG => c
-    if (rd.RepAt(Reputation::G, Reputation::G, Action::C) != Reputation::G ) { return false; }
-    if (ar.ActAt(Reputation::G, Reputation::G) != Action::C) { return false; }
-    // GGd => B
-    if (rd.RepAt(Reputation::G, Reputation::G, Action::D) == Reputation::G) { return false; }
-    else {
-      if (rd.RepAt(Reputation::G, Reputation::G, Action::D) == Reputation::N) {
-        rd = rd.Permute({1,0,2});  // swap N and B. Regard N as B in the following.
-        ar = ar.Permute({1,0,2});
-      }
-    }
-    // GB => d
-    if (ar.ActAt(Reputation::G, Reputation::B) != Action::D) { return false; }
-
-    // BGc => G and BG => c
-    if (rd.RepAt(Reputation::B, Reputation::G, Action::C) != Reputation::G) { return false; }
-    if (ar.ActAt(Reputation::B, Reputation::G) != Action::C) { return false; }
-
-    // BGd => not G
-    if (rd.RepAt(Reputation::B, Reputation::G, Action::D) == Reputation::G) { return false; }
-
-    return true;
-
-  }
   v3d_t ResidentEqReputation() { CalcHStarResident(); return resident_h_star; } // equilibrium reputation of resident species
   double ResidentCoopProb() { CalcHStarResident(); return resident_coop_prob; } // cooperation probability between resident species without implementation error
   v3d_t HStarMutant(const ActionRule& mutant_action_rule) {
