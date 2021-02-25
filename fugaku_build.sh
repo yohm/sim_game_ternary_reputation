5fac266d8a571c4806e18737f02a85793b2362f0
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-02-25 13:43:16 +0900

    updated scripts for fugaku

--- fugaku_build.sh
+++ fugaku_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
-mpiFCCpx -Nclang -std=c++14 -Kfast -Kopenmp -DNDEBUG -o main_search_ESS.out main_search_ESS.cpp
+mpiFCCpx -Nclang -std=c++14 -Kfast -Kopenmp -DNDEBUG -Ijson/include -Iicecream -Icaravan-lib -o main_search_ESS.out main_search_ESS.cpp
 
