68d5077b6200798e6529d4e7de73488d3a6af215
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-02-26 13:30:54 +0900

    updated build script

--- fugaku_build.sh
+++ fugaku_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 
-mpiFCCpx -Nclang -std=c++14 -Kfast -Kopenmp -DNDEBUG -Ijson/include -Iicecream -Icaravan-lib -o main_search_ESS.out main_search_ESS.cpp
+mpiFCCpx -Nclang -std=c++14 -Kfast -Kopenmp -DNDEBUG -I$HOME/sandbox/eigen-3.3.7 -Ijson/include -Iicecream -Icaravan-lib -o main_search_ESS.out main_search_ESS.cpp
 
