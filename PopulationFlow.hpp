9a8bca8aff2b07798060178a962d21b757393eaf
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-05-21 13:28:10 +0900

    changed the specification of population flow

--- PopulationFlow.hpp
+++ PopulationFlow.hpp
@@ -5,16 +5,19 @@
 #include <iomanip>
 #include "Game.hpp"
 
+
 class PopulationFlow {
   public:
-  // calculate the flow of the players between reputations
-  // we consider a multi-graph whose nodes are {B,N,G} and edges are {XY[cd]} where X,Y are reputations.
+  // calculate the detailed flow of the players between reputations
+  // we consider a multi-graph whose nodes are {B,N,G} and edges represent amount of flow T_{XYa->Z}.
+  // we don't take sum over Y
+  // w[X,Y,act,Z]
 
-  PopulationFlow(const Game& g) {
+  PopulationFlow(const Game &_g) : g(_g) {
     for (size_t i = 0; i < w.size(); i++) { w[i] = 0.0; }
+    for (size_t i = 0; i < 54; i++) { assert( Idx( ToReps(i) ) == i ); }
 
     std::array<double,3> h_star = g.ResidentEqReputation();
-
     for (int i = 0; i < 3; i++) {
       Reputation X = static_cast<Reputation>(i);
       for (int j = 0; j < 3; j++) {
@@ -24,124 +27,74 @@ class PopulationFlow {
           Reputation Z = static_cast<Reputation>(k);
           if (act == Action::C) {
             if (g.strategy.rd.RepAt(X,Y,Action::C) == Z) {
-              w[Idx(X,Z,Action::C)] += h_star[i] * h_star[j] * (1.0-g.mu_e) * (1.0-g.mu_a);
+              w[Idx(X,Y,Action::C,Z)] += h_star[i] * h_star[j] * (1.0-g.mu_e) * (1.0-g.mu_a);
             }
             else {
-              w[Idx(X,Z,Action::C)] += h_star[i] * h_star[j] * (1.0-g.mu_e) * 0.5 * g.mu_a;
+              w[Idx(X,Y,Action::C,Z)] += h_star[i] * h_star[j] * (1.0-g.mu_e) * 0.5 * g.mu_a;
             }
             if (g.strategy.rd.RepAt(X,Y,Action::D) == Z) {
-              w[Idx(X,Z,Action::D)] += h_star[i] * h_star[j] * g.mu_e * (1.0-g.mu_a);
+              w[Idx(X,Y,Action::D,Z)] += h_star[i] * h_star[j] * g.mu_e * (1.0-g.mu_a);
             }
             else {
-              w[Idx(X,Z,Action::D)] += h_star[i] * h_star[j] * g.mu_e * 0.5 * g.mu_a;
+              w[Idx(X,Y,Action::D,Z)] += h_star[i] * h_star[j] * g.mu_e * 0.5 * g.mu_a;
             }
           }
           else {  // act == Action::D
             if (g.strategy.rd.RepAt(X,Y,Action::D) == Z) {
-              w[Idx(X,Z,Action::D)] += h_star[i] * h_star[j] * (1.0-g.mu_a);
+              w[Idx(X,Y,Action::D,Z)] += h_star[i] * h_star[j] * (1.0-g.mu_a);
             }
             else {
-              w[Idx(X,Z,Action::D)] += h_star[i] * h_star[j] * 0.5 * g.mu_a;
+              w[Idx(X,Y,Action::D,Z)] += h_star[i] * h_star[j] * 0.5 * g.mu_a;
             }
           }
         }
       }
     }
   }
-
-  double Get(Reputation donor, Reputation recip, Action act) const {
-    return w[ Idx(donor, recip, act) ];
-  }
-  const std::array<double,18>& GetAll() const { return w; }
-
-  std::string Inspect(bool simple = true) const {
-    std::stringstream ss;
-    ss << std::fixed << std::setprecision(4);
-    // calc major flow
-    ss << "--- MAJOR FLOW ---" << std::endl;
-    std::array< std::pair<double,size_t>, 18> flow_idx;
-    for (size_t i = 0; i < 18; i++) {
-      flow_idx[i] = std::make_pair(w[i], i);
-    }
-    std::sort(flow_idx.begin(), flow_idx.end());
-    std::reverse(flow_idx.begin(), flow_idx.end());
-    for (auto f_idx: flow_idx) {
-      if (f_idx.first < 0.01) break;
-      ss << RepString(f_idx.second) << ": " << std::setw(6) << f_idx.first << "   ";
-    }
-    ss << std::endl;
-
-    if (!simple) {
-      ss << "--- DETAIL ---\n";
-
-      for (size_t i = 0; i < 18; i++) {
-        ss << RepString(i) << ": " << std::setw(6) << w[i];
-        ss << ((i % 6 == 5) ? "\n" : "   ");
-      }
-    }
-    return ss.str();
-  }
   std::string InspectMD() const {
     std::stringstream ss;
     ss << std::fixed << std::setprecision(3);
     // calc major flow
-    ss << "- Major Population Flow:" << std::endl;
-    std::array<std::pair<double, size_t>, 18> flow_idx;
-    for (size_t i = 0; i < 18; i++) {
+    double threshold = 0.004;
+    ss << "- Major Population Flow: (threshold=" << threshold << ")" << std::endl;
+    std::array<std::pair<double, size_t>, 54> flow_idx;
+    for (size_t i = 0; i < 54; i++) {
       flow_idx[i] = std::make_pair(w[i], i);
     }
     std::sort(flow_idx.begin(), flow_idx.end());
     std::reverse(flow_idx.begin(), flow_idx.end());
     for (auto f_idx: flow_idx) {
-      if (f_idx.first < 0.01) break;
+      if (f_idx.first < threshold) break;
       ss << "  - " << RepString(f_idx.second) << ": " << f_idx.first << "\n";
     }
     ss << std::endl;
     return ss.str();
   }
+  private:
+  const Game g;
+  std::array<double,54> w;
+  size_t Idx(Reputation X, Reputation Y, Action act, Reputation Z) const { // return index for (X,Y,a,Z)
+    return static_cast<int>(X)*18 + static_cast<int>(Y)*6 + static_cast<int>(act)*3 + static_cast<int>(Z);
+  }
+  std::tuple<Reputation,Reputation,Action,Reputation> ToReps(size_t i) const {
+    if (i > 54) { throw std::runtime_error("invalid index"); }
+    return std::make_tuple(static_cast<Reputation>(i/18), static_cast<Reputation>((i/6)%3), static_cast<Action>((i/3)%2), static_cast<Reputation>(i%3) );
+  }
   std::string RepString(size_t i) const {
     std::stringstream ss;
     const auto tup = ToReps(i);
-    Reputation X = std::get<0>(tup), Y = std::get<1>(tup);
+    Reputation X = std::get<0>(tup), Y = std::get<1>(tup), Z = std::get<3>(tup);
     Action a = std::get<2>(tup);
-    ss << "(" << X << "-" << a << "-" << Y << ")";
+    ss << "(" << X << Y << ":" << a << Z << ")";
+
+    const auto no_error = g.strategy.At(X,Y);
+    const Action correct_act = std::get<0>(no_error);
+    const Reputation correct_rep = std::get<1>(no_error);
+
+    if (correct_act == a && correct_rep == Z) ss << "  "; // no error occurred
+    else if (correct_act == a) ss << "+ "; // only assessment error occurred
+    else if (correct_act != a && std::get<2>(no_error) == Z) ss << "* ";  // only implementation error occurred
+    else ss << "*+"; // both errors occurred
     return ss.str();
   }
-
-  std::pair<double,size_t> MaxD(const PopulationFlow& other) const {
-    double ans = 0.0;
-    size_t idx = 0;
-    for (size_t i = 0; i < 18; i++) {
-      double d = std::abs(w[i] - other.w[i]);
-      if (d > ans) { ans = d; idx = i; }
-    }
-    return std::make_pair(ans, idx);
-  }
-
-  double D1(const PopulationFlow& other) const {
-    double ans = 0.0;
-    for (int i = 0; i < 18; i++) {
-      ans += std::abs(w[i] - other.w[i]);
-    }
-    return ans;
-  }
-
-  double D2(const PopulationFlow& other) const {
-    double ans = 0.0;
-    for (int i = 0; i < 18; i++) {
-      ans += (w[i] - other.w[i]) * (w[i] - other.w[i]);
-    }
-    return ans;
-  }
-
-  private:
-  std::array<double,18> w;
-  size_t Idx(Reputation donor, Reputation recip, Action act) const { // return index for (X,Y,a)
-    return static_cast<int>(donor) * 6 + static_cast<int>(recip) * 2 + static_cast<int>(act);
-  }
-  std::tuple<Reputation,Reputation,Action> ToReps(size_t i) const {
-    if (i > 18) { throw std::runtime_error("invalid index"); }
-    return std::make_tuple(static_cast<Reputation>(i/6), static_cast<Reputation>((i/2)%3), static_cast<Action>(i%2));
-  }
 };
-
