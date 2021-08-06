58201e36c0d326cf94e1642146c74db4430c645d
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-08-06 15:11:29 +0900

    update script for plot

--- python/plot_num_ESS.py
+++ python/plot_num_ESS.py
@@ -22,27 +22,37 @@ plt.rcParams['figure.facecolor'] = 'white'
 
 # %%
 import subprocess
+from subprocess import PIPE
+import os,sys
+sys.path.append( os.environ['OACIS_ROOT'] )
+import oacis
 
+#%%
+sim = oacis.Simulator.find_by_name('ternary_reputation_search')
+base_ps = sim.find_parameter_set( {'benefit':1.1, 'cost':1.0, 'mu_e':0.001, 'mu_a':0.001, 'coop_prob_th':0.99} )
+ps_list = base_ps.parameter_sets_with_different('benefit')
+list(ps_list)
+
+#%%
 nls = []
-bs = ['1.1', '1.2', '1.5', '2.0', '2.5', '3.0', '4.0', '5.0']
-for b in bs:
-    nl = int(subprocess.run(['wc', '-l', f"../,result/job_b{b}/ESS_ids"], capture_output=True).stdout.split()[0])
+bs = []
+for ps in ps_list:
+    run_dir = ps.runs().first().dir()
+    print(f"{run_dir}/ESS_ids")
+    nl = int( subprocess.run(['wc', '-l', f"{run_dir}/ESS_ids"], stdout=PIPE).stdout.split()[0] )
     nls.append(nl)
+    bs.append(ps.v()['benefit'])
 
-nls
+bs,nls
 # %%
 plt.clf()
 bs = [float(x) for x in bs]
-plt.ylim((0,7))
+plt.ylim((0,9))
 plt.xlim((1,5.1))
 plt.xticks([1,2,3,4,5])
 plt.xlabel(r'benefit-to-cost ratio')
 plt.ylabel(r'# of ESSs ($\times 10^6$)')
 plt.plot(bs, np.array(nls)/1e6, 'o-')
+#plt.show()
 plt.savefig("num_ESS.pdf")
-# %%
-n_mu_a = int(subprocess.run(['wc', '-l', f"../,result/job_mu_a_2e-3/ESS_ids"], capture_output=True).stdout.split()[0])
-n_mu_e = int(subprocess.run(['wc', '-l', f"../,result/job_mu_e_2e-3/ESS_ids"], capture_output=True).stdout.split()[0])
-n_mu_e_mu_a = int(subprocess.run(['wc', '-l', f"../,result/job_mu_a_mu_e_2e-3/ESS_ids"], capture_output=True).stdout.split()[0])
-n_mu_a, n_mu_e, n_mu_e_mu_a
 # %%
